ccf543cbc8d19fe4a69918cf17d0832708bf64a6
Glaucia de Pádua <glaudepadua@gmail.com>
2020-02-10 21:24:52 -0300

    Adicionado a estrutura do projeto e layout do front.

---
+++ bridge/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace bridge
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ bridge/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="bridge.MvcApplication" Language="C#" %>
